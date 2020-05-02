c67abf2926f54d0034a0b525de18597f0330a061
Jacob Stovall <jacobemmersonstovall@gmail.com>
2020-05-01 19:54:06 -0700

    Added boundary checks to Bill

--- src/Bill.lua
+++ src/Bill.lua
@@ -13,12 +13,15 @@ function Bill:init()
     self.currentAnimation = billIdleAnimation
     self.y = VIRTUAL_HEIGHT - 16
     self.x = VIRTUAL_WIDTH / 2
-    self.speed = 30
+    self.width = 8
+    self.height = 8
+    self.speed = 200
 end
 
 function Bill:update(dt)
     self.currentAnimation:update(dt)
 
+    --Checking inputs
     billDidSomething = false
     if love.keyboard.isDown("w") then
         self.y = self.y - (self.speed * dt)
@@ -36,10 +39,24 @@ function Bill:update(dt)
         self.x = self.x + (self.speed * dt)
         billDidSomething = true
     end
+    --Checking which animation to use
     if billDidSomething then
         self.currentAnimation = billMovingAnimation
     else
         self.currentAnimation = billIdleAnimation
+    end
+    -- Boundary check
+    if self.x < 0 then
+        self.x = 0
+    end
+    if self.y < 0 then
+        self.y = 0
+    end
+    if self.x + self.width > VIRTUAL_WIDTH then
+        self.x = VIRTUAL_WIDTH - self.width
+    end
+    if self.y + self.height > VIRTUAL_HEIGHT then
+        self.y = VIRTUAL_HEIGHT - self.height
     end
 end
 
